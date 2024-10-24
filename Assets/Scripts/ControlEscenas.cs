16787e864f405ad2b34f1eb62b0eabf3384aef6c
joel tamayo <jueltamayo25@gmail.com>
2024-10-23 23:28:46 -0600

    Actualizacion de mecanicas para cerrar el juego

--- Assets/Scripts/ControlEscenas.cs
+++ Assets/Scripts/ControlEscenas.cs
@@ -6,7 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class ControlEscenas : MonoBehaviour
 {
-    public void abrirEscena(int numeroEscena) {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                // Si estamos en la escena del menú principal, cerrar la aplicación
+                Application.Quit();
+            }
+            else
+            {
+                // Si estamos en otra escena, cargar el menú principal
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+    public void abrirEscena(int numeroEscena)
+    {
         SceneManager.LoadScene(numeroEscena);
     }
 }
